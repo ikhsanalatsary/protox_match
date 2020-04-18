879cfac5251a7f6189bba2b573e30968db1f4cd7
ikhsanalatsary <skull.saders18@gmail.com>
2020-04-18 22:11:05 +0700

    :lock: secure channel with tls

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:grpc/grpc.dart';
 import 'package:protox_match/generated/hello.pbgrpc.dart';
 
@@ -58,7 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<void> _rpc() async {
     final channel = ClientChannel(
-      '192.168.1.2', // change with your local ip address
+      'MacBook-Pro', // change this with your local ip address or with host name
       port: 50051,
       options: const ChannelOptions(credentials: ChannelCredentials.insecure()),
     );
@@ -78,6 +79,134 @@ class _MyHomePageState extends State<MyHomePage> {
       print('Caught error: $e');
     }
     await channel.shutdown();
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(_message),
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+  class HomeSSL extends StatefulWidget {
+   HomeSSL({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _HomeSSLState createState() => _HomeSSLState();
+}
+
+class _HomeSSLState extends State<HomeSSL> {
+ int _counter = 0;
+  String _firstName = 'John';
+  String _lastName = 'Wick';
+  String _message = 'hai';
+  String _host = 'MacBook-Pro';
+  int _port = 50051;
+
+  void initState() {
+    super.initState();
+    _rpc();
+  }
+
+  Future<List<int>> _loadCert() async {
+    final data = await rootBundle.load('certs/ca.crt');
+    final buffer = data.buffer;
+    List<int> certs = buffer.asUint16List();
+
+    return certs;
+  }
+
+  Future<void> _rpc() async {
+    try {
+      final certs = await _loadCert();
+      final channel =
+          ClientChannel(_host, // change with your local ip address
+              port: _port,
+              options: ChannelOptions(
+                credentials: ChannelCredentials.secure(certificates: certs),
+              ));
+
+      final stub = HelloClient(channel);
+      final request = HelloRequest();
+      request..firstName = _firstName;
+      request..lastName = _lastName;
+      final response = await stub.greeting(request);
+      print('Greeter client received: ${response.message}');
+      setState(() {
+        _message = response.message;
+      });
+    } catch (e) {
+      print('Caught error: $e');
+    }
   }
 
   void _incrementCounter() {
